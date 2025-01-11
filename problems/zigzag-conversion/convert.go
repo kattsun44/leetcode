83747822c9c455599bfc8000242568831e8347e0
kattsun44 <kattsun4416@gmail.com>
2025-01-12 07:42:38 +0900

    fix: zigzag-conversion/convert.go

--- problems/zigzag-conversion/convert.go
+++ problems/zigzag-conversion/convert.go
@@ -6,24 +6,34 @@ import (
 )
 
 func convert(s string, numRows int) string {
-	var container [][]string
+	// ジグザグに渡される文字を格納するスライス
+	container := make([][]string, numRows)
+
+	// コンテナを1次元均したスライス
 	var flatten []string
-	pattern := max(numRows*2-2, numRows)
+
+	// 何番目の文字が何列目に格納されるか
 	positions := make(map[int]int)
 
+	// 格納先が変わる周期
+	pattern := max(numRows*2-2, numRows)
+
+	// positions と pattern を対応付け
 	for i := 0; i < pattern; i++ {
 		positions[i] = min(pattern-i, i)
-		container = append(container, nil)
 	}
 
+	// 渡された文字列をコンテナに格納していく
 	for i, v := range s {
 		container[positions[i%pattern]] = append(container[positions[i%pattern]], string(v))
 	}
 
+	// flatten に、スライスごとに連結した文字列を格納
 	for _, v := range container {
 		flatten = append(flatten, strings.Join(v, ""))
 	}
 
+	// flatten を連結した文字列を返却
 	return strings.Join(flatten, "")
 }
 
